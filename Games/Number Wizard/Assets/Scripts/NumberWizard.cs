fa36e48db2c9771722b1bbc269c044c87c47543c
jehunter5811 <justin.edward.hunter@gmail.com>
2017-07-26 19:42:23 -0500

    Pushing to github

---
+++ Games/Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+
+		print ("========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me.");
+
+		print ("The highest number you can pick is " + max);
+		print ("The lowest number you can pick is " + min);
+
+		print ("Is your number higher or lower than " + guess + " ");
+		print ("Up = higher, down = lower, enter = equals.");
+
+		max = max + 1;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up")) {
+//			print ("Up arrow key was pressed");
+			min = guess;
+			NextGuess ();
+		}
+
+		else if (Input.GetKeyDown ("down")) {
+//			print ("Down arrow key was pressed");
+			max = guess;
+			NextGuess ();
+		}
+
+		else if (Input.GetKeyDown ("return")) {
+			print ("I won!");
+			StartGame ();
+		}
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + " ");
+		print ("Up = higher, down = lower, enter = equals.");
+	}
+}
---
+++ Games/Text101/Assets/Scripts/TextController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+	private enum States {opening, start, mirror, sheets_0, lock_0, cell_mirror, sheets_1, locks_1, freedom};
+	private States myState;
+
+	// Use this for initialization
+	void Start () {
+		myState = States.opening;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (myState);
+		if (myState == States.opening) {
+			state_opening ();
+		}else if (myState == States.start) {
+			state_start ();
+		}
+	}
+
+	void state_opening () {
+		text.text = "You and 3 friends are wanted felons, you need " +
+		"enough money to leave the country and start a new life.\n" +
+		"Your job is to get through it all WITHOUT getting caught. Good Luck.\n" +
+		"Press S to Start";
+
+		if (Input.GetKeyDown(KeyCode.S)) {
+				myState = States.start;
+		}
+	}
+
+	void state_start () {
+		text.text = "Time: 3:14 PM \nPlace: Las Vegas, Nevada\nYou've just arrived near the bank.\n" +
+					"Are you guys going in with masks on or masks off?\nPress M for Fuck it, Masks Off or press O for Masks on";
+
+		if (Input.GetKeyDown(KeyCode.R)) {
+			myState = States.opening;
+		}
+	}
+}
